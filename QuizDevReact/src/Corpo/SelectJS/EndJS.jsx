99f767487188856e00c2de8823ca87eee9ae0cdf
Thyale MD <thyaledantas@gmail.com>
2024-05-15 14:41:18 -0300

    Create EndJS.jsx

---
+++ QuizDevReact/src/Corpo/SelectJS/EndJS.jsx
@@ -0,0 +1,11 @@
+function EndJS(){
+    return(
+        <div className="EndJS">
+            <h1>O seu quiz de JavaScript chegou ao fim!</h1>
+            <p>Espero que tenha gostado!</p>
+            <p>Até a próxima...</p>
+        </div>
+    )
+}
+
+export default EndJS
